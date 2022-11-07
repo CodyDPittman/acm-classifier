f19283b09b50732afb1b3d566e34d89e8795f87d
jdohe007 <jdohe007@odu.edu>
2022-11-06 22:27:03 -0500

    failed to get pdf reader to work. I tried 3 different methods. They are each commented out. issues with packages. I will need to talk to professor zeil later to try to fix.

--- acm-classifier/src/test/java/edu/odu/cs/cs350/pne/testPDF.java
+++ acm-classifier/src/test/java/edu/odu/cs/cs350/pne/testPDF.java
@@ -10,8 +10,45 @@ public class testPDF{
 	
 @Test
 public void testPDFInput() {
-	//pdfInput input = new pdfInput();
-	//input.main("sample.pdf");
+	/*
+	
+	String argsIn[];
+	argsIn = new String[1];
+	String output[] = new String[100];
+	argsIn[0] = "sample.pdf";
+	pdfInput input = new pdfInput();
+	
+	
+	String  waterlooTest="My, my, at Waterloo, Napoleon did surrender"
+			+ "Oh-yeah (oh-yeah), and I have met my destiny in quite a similar way"
+			+ "The history book on the shelf"
+			+ "Is always repeating itself"
+			+ "Waterloo, I was defeated, you won the war"
+			+ "Waterloo, promise to love you forever more"
+			+ "Waterloo, couldn't escape if I wanted to"
+			+ "Waterloo, knowing my fate is to be with you"
+			+ "Wa-wa-wa-wa, waterloo, finally facing my waterloo"
+			+ "My, my, I tried to hold you back but you were stronger"
+			+ "Oh-yeah (oh-yeah), and now it seems my only chance is giving up the fight"
+			+ "And how could I ever refuse?"
+			+ "I feel like I win when I lose"
+			+ "Waterloo, I was defeated, you won the war"
+			+ "Waterloo, promise to love you for ever more"
+			+ "Waterloo, couldn't escape if I wanted to"
+			+ "Waterloo, knowing my fate is to be with you"
+			+ "Wa-wa-wa-wa, waterloo, finally facing my waterloo"
+			+ "So how could I ever refuse?"
+			+ "I feel like I win when I lose"
+			+ "Waterloo, couldn't escape if I wanted to"
+			+ "Waterloo, knowing my fate is to be with you"
+			+ "Wa-wa-wa-wa, waterloo, finally facing my waterloo"
+			+ "Ooh-ooh-ooh, waterloo, knowing my fate is to be with you"
+			+ "Wa-wa-wa-wa, waterloo, finally facing my waterloo"
+			+ "Ooh-ooh-ooh, waterloo";
+	String[] arr = waterlooTest.split(" ");
+	
+	assertEquals(output = input.readPDFInput(argsIn), waterlooTest);
+	*/
 }
 
 
