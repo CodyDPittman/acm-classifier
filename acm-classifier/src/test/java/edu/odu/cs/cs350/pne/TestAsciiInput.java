9c8a72a239ddb2101a73c160d1470724172f84b2
Al1374525 <albertayiteyadjin@gmail.com>
2022-11-21 01:22:32 -0500

    test 3 (i belive)

--- acm-classifier/src/test/java/edu/odu/cs/cs350/pne/TestAsciiInput.java
+++ acm-classifier/src/test/java/edu/odu/cs/cs350/pne/TestAsciiInput.java
@@ -2,8 +2,16 @@ package edu.odu.cs.cs350.pne;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Currency;
 
 public class TestAsciiInput{
 
@@ -12,6 +20,7 @@ public class TestAsciiInput{
     @Test
     public void testaddFile(){
         AsciiInput input = new AsciiInput();
+        input.addFile(FileName);
         assertThat(input.addFile(FileName), equalTo(FileName));
         
 
@@ -19,6 +28,20 @@ public class TestAsciiInput{
 
     @Test
     public void testReadToFile(){
+    AsciiInput reader = new AsciiInput();
+    reader.ReadToFile();
+    try(BufferedReader br = new BufferedReader(new FileReader(FileName))) {
+        
+    } catch (Exception e) {
+        String currLine;
+        e.printStackTrace();
+        assertFalse(false);
+        // TODO: handle exception
         
     }
-}
+       
+    }
+    }
+
+    
+
