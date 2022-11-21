b660ddd65d3779caa64f49466a0ae3006c0c1549
Al1374525 <albertayiteyadjin@gmail.com>
2022-11-21 01:35:27 -0500

    production3 ( i believe)

--- acm-classifier/src/main/java/edu/odu/cs/cs350/pne/AsciiInput.java
+++ acm-classifier/src/main/java/edu/odu/cs/cs350/pne/AsciiInput.java
@@ -10,9 +10,27 @@ public class AsciiInput {
 
     private static final String FileName = "C:\\test\\test.txt";
 
-    public Object addFile(String filename) {
+    /*May use public Object addFile(String filename) if needed */
+    public String addFile(String filename) {
         return FileName;
     }
+
+    public void ReadToFile(){
+    try(BufferedReader br = new BufferedReader(new FileReader(FileName))) {
+        String  currLine;
+        while((currLine = br.readLine()) != null){
+            System.out.println(currLine);
+        }
+        
+    } catch (Exception e) {
+
+        // TODO: handle exception
+    }
+
+
+    }
+
+     
 
     
     
