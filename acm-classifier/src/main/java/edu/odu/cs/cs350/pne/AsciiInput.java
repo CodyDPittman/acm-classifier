863dc2f2e29cc211ce836ebb4cf9911e48b4e67d
Al1374525 <albertayiteyadjin@gmail.com>
2022-11-30 15:32:30 -0500

    production

--- acm-classifier/src/main/java/edu/odu/cs/cs350/pne/AsciiInput.java
+++ acm-classifier/src/main/java/edu/odu/cs/cs350/pne/AsciiInput.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
  
 
 public class AsciiInput {
@@ -16,7 +17,7 @@ public class AsciiInput {
     }
 
     public static void main(String[] args){
-    try(BufferedReader br = new BufferedReader(new FileReader(FileName))) {
+        try(BufferedReader br = new BufferedReader(new FileReader(FileName))) {
         String  currLine;
         while((currLine = br.readLine()) != null){
             System.out.println(currLine);
@@ -26,10 +27,12 @@ public class AsciiInput {
         e.printStackTrace();
 
         // TODO: handle exception
-    }
+    
 
 
+
     }
+}
 
      
 
